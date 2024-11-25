f32d866a319b8e3e0077c908244b640029efcf29
Nue666 <ayrton132@yandex.ru>
2024-11-25 13:27:37 +0400

    Add files via upload

--- module_2_hard.py
+++ module_2_hard.py
@@ -4,6 +4,7 @@ def password(num):
         for j in range(2,num+1):
             if num % (i + j) == 0 and (str(j)+str(i)) not in result and i != j:
                 result.append(str(i)+str(j))
+    print(f'{num} - число первой вставки\nнужный пароль:')
     print(*result, sep = '')
 
-password(8)
+password(int(input()))
