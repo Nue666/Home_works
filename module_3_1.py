36177a6fc400d54009856faaa09d3efb050441c2
Nue666 <ayrton132@yandex.ru>
2024-12-02 23:21:53 +0400

    Add files via upload

--- module_3_1.py
+++ module_3_1.py
@@ -1,30 +1,28 @@
 def is_constains(*args):
-    global calls
-    calls += 1
+    count_calls()
     count = 0
     for i in range(len(args[1])):
         if args[0].lower() == args[1][i].lower():
-            print(True)
+            return True
             break
         else:
             count += 1
     if count == len(args[1]):
-        print(False)
-
+        return False
 
 def string_info(arg):
-    global calls
-    calls += 1
-    print(tuple([len(arg), arg.upper(), arg.lower()]))
+    count_calls()
+    return tuple([len(arg), arg.upper(), arg.lower()])
+
 
 
 def count_calls():
-    print(calls)
-
+    global calls
+    calls += 1
 
 
 calls = 0
-string_info(input())
-is_constains(input(), ['PascaL', 'pYtHon', 'Java', 'CsS'])
-is_constains('Urban', ['ban', 'BaNaN', 'urbAn', 'lohi'])
-count_calls()
+print(string_info(input()))
+print(is_constains(input(), ['PascaL', 'pYtHon', 'Java', 'CsS']))
+print(is_constains('Urban', ['ban', 'BaNaN', 'urbAn', 'lohi']))
+print(calls)
