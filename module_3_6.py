e7b37e735bf2dfcc8ba8e671d9378b961b1f9463
Nue666 <ayrton132@yandex.ru>
2024-12-04 01:57:22 +0400

    Add files via upload

--- module_3_6.py
+++ module_3_6.py
@@ -1,14 +1,14 @@
-def get_multiplied_digts(func, num):
+def proverka(func, num):
     if num == 0:
         return 0
     else:
-        return get_multiplied_digt(num)
+        return get_multiplied_digts(num)
 
-def get_multiplied_digt(num):
+def get_multiplied_digts(num):
     str_number = str(num)
     first = int(str_number[0])
     if len(str_number) > 1:
-        return first * get_multiplied_digt(int(str_number[1:]))
+        return first * get_multiplied_digts(int(str_number[1:]))
     elif first == 0:
         return 1
     else:
@@ -16,11 +16,11 @@ def get_multiplied_digt(num):
 
 
 
-result = get_multiplied_digts(get_multiplied_digt(402030), 402030)
-result2 = get_multiplied_digts(get_multiplied_digt(30303), 30303)
-result3 = get_multiplied_digts(get_multiplied_digt(0), 0)
-result4 = get_multiplied_digts(get_multiplied_digt(1), 1)
-result5 = get_multiplied_digts(get_multiplied_digt(102030405060708090), 102030405060708090)
+result = proverka(get_multiplied_digts(402030), 402030)
+result2 = proverka(get_multiplied_digts(30303), 30303)
+result3 = proverka(get_multiplied_digts(0), 0)
+result4 = proverka(get_multiplied_digts(1), 1)
+result5 = proverka(get_multiplied_digts(102030405060708090), 102030405060708090)
 
 print(result)
 print(result2)
