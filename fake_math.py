c2e4aab4ea225ccf3088e5457e934e9562c6b3ba
Nue666 <ayrton132@yandex.ru>
2024-12-10 00:38:51 +0400

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,5 @@
+def divide(num1, num2):
+    if num2 == 0:
+        return 'Ошибка'
+    else:
+        return num1 / num2
---
+++ true_math.py
@@ -0,0 +1,6 @@
+from math import inf
+def divide(num1, num2):
+    if num2 == 0:
+        return inf
+    else:
+        return num1 / num2
